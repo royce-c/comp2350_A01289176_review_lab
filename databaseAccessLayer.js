81015e1550f18dcfd7b31702accd4db186a111e7
Royce Coviello <coviello.royce@gmail.com>
2023-03-02 00:24:36 -0800

    added add and delete restaurant

--- databaseAccessLayer.js
+++ databaseAccessLayer.js
@@ -15,24 +15,24 @@ function getAllRestaurants(callback) {
 	});
 }
 
-// function addPerson(postData, callback) {
-// 	let sqlInsert = "INSERT INTO person (first_name, last_name) VALUES (:fname, :lname);";
-// 	let params = {	
-// 			fname: postData.first_name,
-// 			lname: postData.last_name
-// 		};
-// 	console.log(sqlInsert);
-// 	database.query(sqlInsert, params, (err, results, fields) => {
-// 		if (err) {
-// 			console.log(err);
-// 			callback(err, null);
-// 		}
-// 		else {
-// 			console.log(results);
-// 			callback(null, results);
-// 		}
-// 	});
-// }
+function addRestaurant(postData, callback) {
+	let sqlInsert = "INSERT INTO restaurant (restaurant_name, description) VALUES (:restaurant_name, :description);";
+	let params = {	
+		restaurant_name: postData.restaurant_name,
+			description: postData.description
+		};
+	console.log(sqlInsert);
+	database.query(sqlInsert, params, (err, results, fields) => {
+		if (err) {
+			console.log(err);
+			callback(err, null);
+		}
+		else {
+			console.log(results);
+			callback(null, results);
+		}
+	});
+}
 
 // function deletePersonSkill(personId, callback) {
 // 	let sqlDeletePersonSkill = "delete from person_skill where person_id = :personID";
@@ -51,21 +51,21 @@ function getAllRestaurants(callback) {
 // 	});	
 // }
  
-// function deletePerson(personId, callback) {
-// 	let sqlDeletePerson = "delete from person where person_id = :personID";
-// 	let params = {
-// 		personID: personId
-// 	};
-// 	console.log(sqlDeletePerson);
-// 	database.query(sqlDeletePerson, params, (err, results, fields) => {
-// 		if (err) {
-// 			callback(err, null);
-// 		}
-// 		else {
-// 			console.log(results);
-// 			callback(null, results);
-// 		}		
-// 	});	
-// }
+function deleteRestaurant(restaurantId, callback) {
+	let sqlDeleteRestaurant = "delete from restaurant where restaurant_id = :restaurantID";
+	let params = {
+		restaurantID: restaurantId
+	};
+	console.log(sqlDeleteRestaurant);
+	database.query(sqlDeleteRestaurant, params, (err, results, fields) => {
+		if (err) {
+			callback(err, null);
+		}
+		else {
+			console.log(results);
+			callback(null, results);
+		}		
+	});	
+}
 
-module.exports = {getAllRestaurants}
+module.exports = {getAllRestaurants, addRestaurant, deleteRestaurant}
