97b26c8d2c5439cde19005a41a3ca709446cf807
Royce Coviello <coviello.royce@gmail.com>
2023-03-01 23:17:03 -0800

    db added

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,27 +1,27 @@
 const mysql = require('mysql2');
 
-const is_heroku = process.env.IS_HEROKU || false;
+const is_qoddi = process.env.IS_QODDI || false;
 
-const dbConfigHeroku = {
-	host: "us-cdbr-east-03.cleardb.com",
-	user: "b1ab7fb2ee03bc",
-	password: "2a484a2d",
-	database: "heroku_3d208ad4bd6f421",
+const dbConfigQoddi = {
+	host: "sql.freedb.tech",
+	user: "freedb_maind",
+	password: "8DK%aWcE#QdE5sk",
+	database: "freedb_comp2350-A01289176",
 	multipleStatements: false,
 	namedPlaceholders: true
 };
 
 const dbConfigLocal = {
-	host: "localhost",
+	host: "localhost:3306",
 	user: "root",
-	password: "Password",
-	database: "skill",
+	password: "",
+	database: "restaurant_review",
 	multipleStatements: false,
 	namedPlaceholders: true
 };
 
-if (is_heroku) {
-	var database = mysql.createPool(dbConfigHeroku);
+if (is_qoddi) {
+	var database = mysql.createPool(dbConfigQoddi);
 }
 else {
 	var database = mysql.createPool(dbConfigLocal);
